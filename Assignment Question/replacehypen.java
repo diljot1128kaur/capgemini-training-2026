86fffda419ef82b6bc8c243887a3d30bddfce1c1
diljot <diljotkamboj2811@gmail.com>
2026-01-15 21:22:46 +0530

    15 january done

--- Assignment Question/replacehypen.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class replacehypen{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
-        str=str.replace(" ", "_");
-        System.out.println(str);
-    }
-}
